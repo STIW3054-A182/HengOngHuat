50670a242e90562d458216bc8a64ca1e75e912bd
shyyahcheang <47626271+shyyahcheang@users.noreply.github.com>
2019-05-14 02:18:45 +0800

    Add files via upload

---
+++ src/main/java/com/groupProject/CheckValidTable.java
@@ -0,0 +1,64 @@
+package com.groupProject;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.util.Date;
+import java.util.concurrent.Callable;
+
+public class CheckValidTable implements Callable {
+
+    private String validTableLink;
+    private String link;
+
+    public CheckValidTable(String link) {
+        this.link = link;
+    }
+
+    @Override
+    public String call() {
+        long startTime, endTime, executeTime;
+        String date = new Date().toString();
+        String logFile = "myLogFile.log";
+        File file = new File(logFile);
+        FileWriter fw;
+        BufferedWriter bw;
+
+        try {
+            startTime = System.currentTimeMillis();
+            if (ValidTable(link)) {
+                validTableLink = link;
+            } else {
+                endTime = System.currentTimeMillis();
+                executeTime = endTime - startTime;
+
+                fw = new FileWriter(file, true); //appends to file
+                bw = new BufferedWriter(fw);
+                bw.write(date + "\t");
+                bw.write(Thread.currentThread().getName() + " --> " + link + " (not exist player)");
+                bw.write(System.lineSeparator());
+                bw.write("Execution time in milliseconds: " + executeTime);
+                bw.write(System.lineSeparator());
+                bw.close();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return validTableLink;
+    }
+
+    public boolean ValidTable(String url) {
+        Document doc;
+        try {
+            doc = Jsoup.connect(url).get();
+            if (doc.select("table.CRs1 tr").isEmpty())
+                return false;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return true;
+    }
+}
---
+++ src/main/java/com/groupProject/ResultCountPlayer.java
@@ -0,0 +1,50 @@
+package com.groupProject;
+
+import java.util.Vector;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class ResultCountPlayer extends ValidTableLink implements Runnable {
+
+    private Vector<Integer> totalPlayer = new Vector<>();
+    private Future<Vector> future3;;
+    private int resultTotal = 0;
+
+    public ResultCountPlayer() {
+
+    }
+
+    public ResultCountPlayer(Vector<String> existList, String [] existLinkList, Vector<String> existTableList, String [] existTableLinkList) {
+        super(existList, existLinkList, existTableList, existTableLinkList);
+    }
+
+    @Override
+    public void run() {
+        int coreCount = Runtime.getRuntime().availableProcessors();
+        ExecutorService service = Executors.newFixedThreadPool(coreCount);
+
+        System.out.printf("| %-8s | %-6s|\n", "Category", "Total");
+        System.out.printf("| %-8s | %-6s|\n", "--------", "-----");
+
+        for (int c = 0; c < getExistTableList().size(); c++) {
+            String [] tableLink = getExistTableLinkList();
+            CountPlayer countPlayer = new CountPlayer(tableLink[c]);
+            future3 = service.submit(countPlayer);
+            try {
+                totalPlayer = future3.get();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            }
+        }
+
+        for (Integer totalPlayer:totalPlayer) {
+            resultTotal += totalPlayer;
+        }
+        System.out.printf("| %-8s | %-5d |\n","Total",resultTotal);
+        service.shutdown();
+    }
+}
