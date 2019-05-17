20c5541c593761deb2d6f343797984d634ec0acc
tanyujia <47684496+tanyujia@users.noreply.github.com>
2019-05-18 01:07:03 +0800

    Add files via upload

---
+++ GroupAsg(18th May)/src/main/java/MainClass.java
@@ -0,0 +1,31 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Properties;
+
+public class MainClass {
+    public static void main (String[] args) throws IOException {
+
+        String path, fileName;
+        PropertyFileWriting obj = new PropertyFileWriting();
+        Properties write = obj.setProperties();
+        path = write.getProperty("path");
+        fileName = write.getProperty("txtFile");
+        System.out.println("Path: " + path);
+        System.out.println("FileName: " + fileName);
+
+        System.out.println("**************************** DISPLAY VALID LINKS ****************************");
+        new FileWriter("myLogFile.log", false); //overwrites file
+        ValidLink validLink = new ValidLink();
+        validLink.allLink();
+
+        System.out.println("\n************************ DISPLAY ALL TOP 3 PLAYERS FROM EACH CATEGORY ************************");
+        ResultTop3Player topPlayer = new ResultTop3Player();
+        Thread t4 = new Thread(topPlayer);
+        t4.start();
+        try {
+            t4.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ GroupAsg(18th May)/src/main/java/PropertyFileWriting.java
@@ -0,0 +1,29 @@
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.Properties;
+
+public class PropertyFileWriting {
+    public Properties setProperties() {
+        Properties prop = new Properties();
+        String filePath = "src\\main\\resources\\myConfig.properties";
+
+        try (Writer inputStream = new FileWriter(filePath)) {
+            //set the properties value
+            prop.setProperty("path","src\\main\\resources\\");
+            prop.setProperty("txtFile","URL.txt");
+            prop.setProperty("State","KEDAH");
+            prop.setProperty("Top1","1");
+            prop.setProperty("Top2","2");
+            prop.setProperty("Top3","");
+            prop.setProperty("Name","Rosli Iman Hasif");
+
+            //save properties to project root folder
+            prop.store(inputStream,null);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return prop;
+    }
+}
