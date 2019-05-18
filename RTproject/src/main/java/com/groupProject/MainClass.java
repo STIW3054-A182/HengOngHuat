2ae9dbc1c39a94f726579877690b15cbcbd9b81c
limxinyii <47627068+limxinyii@users.noreply.github.com>
2019-05-18 17:34:45 +0800

    Add files via upload

---
+++ RTproject/src/main/java/com/groupProject/MainClass.java
@@ -0,0 +1,71 @@
+package com.groupProject;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Properties;
+
+public class MainClass {
+    public static void main (String[] args) throws IOException {
+
+        /*String path, fileName;
+        PropertyFileWriting obj = new PropertyFileWriting();
+        Properties write = obj.setProperties();
+        path = write.getProperty("path");
+        fileName = write.getProperty("txtFile");
+        System.out.println("Path: " + path);
+        System.out.println("FileName: " + fileName);*/
+
+        System.out.println("**************************** DISPLAY VALID LINKS ****************************");
+        new FileWriter("myLogFile.log", false); //overwrites file
+        ValidLink validLink = new ValidLink();
+        validLink.allLink();
+
+        System.out.println("\n************************** COUNT NUMBER OF PLAYERS **************************");
+        ValidTableLink validTableLink = new ValidTableLink(); //check the link which the category has more than 1 and store it by array
+        validTableLink.existTableLink();
+        ResultCountPlayer resultCountPlayer = new ResultCountPlayer();
+        Thread t1 = new Thread(resultCountPlayer);
+        t1.start();
+        try {
+            t1.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        /*System.out.println("\n***************************** DISPLAY STATISTICS ****************************");
+        ResultStateStatistic stateStatistic = new ResultStateStatistic();
+        Thread t2 = new Thread(stateStatistic);
+        t2.start();
+        try {
+            t2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }*/
+
+        System.out.println("\n******************************* DISPLAY ALL PLAYERS FROM KEDAH *******************************");
+        ResultKedahPlayer kedahPlayers = new ResultKedahPlayer();
+        Thread t3 = new Thread(kedahPlayers);
+        t3.start();
+        try {
+            t3.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        /*System.out.println("\n**************************** COUNT WINNING POINTS ****************************");
+        ResultPointerStatistic pointsStatistic = new ResultPointerStatistic();
+        Thread t4 = new Thread(pointsStatistic);
+        t4.start();
+        try {
+            t4.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }*/
+
+        /*System.out.println("\n************************** DISPLAY A PLAYER RESULT ***************************");
+        NamePlayer namePlayer = new NamePlayer();
+        namePlayer.run();
+        ResultNamePlayer result = new ResultNamePlayer();
+        result.run();*/
+    }
+}
