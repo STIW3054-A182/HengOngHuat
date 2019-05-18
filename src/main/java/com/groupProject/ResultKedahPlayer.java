8ee73b6189773333e0383b62d398f8cef17ddfbd
limxinyii <47627068+limxinyii@users.noreply.github.com>
2019-05-18 16:51:15 +0800

    Update ResultKedahPlayer.java

--- src/main/java/com/groupProject/ResultKedahPlayer.java
+++ src/main/java/com/groupProject/ResultKedahPlayer.java
@@ -23,3 +23,27 @@ public class ResultKedahPlayer extends ValidTableLink implements Runnable {
      * @param existTableLinkList valid table link list
      */
 
+    public ResultKedahPlayer(Vector<String> existList, String [] existLinkList, Vector<String> existTableList, String [] existTableLinkList) {
+        super(existList, existLinkList, existTableList, existTableLinkList);
+    }
+
+
+    @Override
+    public void run() {
+        int coreCount = Runtime.getRuntime().availableProcessors();
+        ExecutorService service = Executors.newFixedThreadPool(coreCount);
+
+        String format = "| %-5s | %-5s | %-35s| %-8s| %-8s| %-8s| %-8s|\n";
+        System.out.format(format, "RK", "SNo", "Name", "Rtg", "State", "Pts", "Category");
+        String format2 = "| %-5s | %-5s | %-35s| %-8s| %-8s| %-8s| %-8s|\n";
+        System.out.format(format2, "-----", "-----", "-----------------------------------",
+                "--------", "--------", "--------", "--------");
+
+        for (int z = 0; z < getExistTableList().size(); z++) {
+            String [] tableLink2 = getExistTableLinkList();
+            Thread myThread = new Thread(new KedahPlayers(tableLink2[z]));
+            service.execute(myThread);
+        }
+        service.shutdown();
+    }
+}
