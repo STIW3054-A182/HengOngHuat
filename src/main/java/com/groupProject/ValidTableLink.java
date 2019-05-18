dbfb092e7517b91a3a4d6a087dbc336a44a8b2a9
ChongMeiYong <47626352+ChongMeiYong@users.noreply.github.com>
2019-05-18 23:54:31 +0800

    Update ValidTableLink.java

--- src/main/java/com/groupProject/ValidTableLink.java
+++ src/main/java/com/groupProject/ValidTableLink.java
@@ -6,6 +6,13 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
+/**
+ * Create ValidTableLink extends from ValidLink
+ * @author Chong Mei Yong
+ * @version 1.0
+ * @since 2019-04-19
+ */
+
 public class ValidTableLink extends ValidLink {
 
     private static Vector<String> existTableList = new Vector<>();
@@ -15,16 +22,31 @@ public class ValidTableLink extends ValidLink {
 
     }
 
+    /**
+     * This constructs a result of total player with a specified existList,existLinkList,existTableList,existTableLinkList.
+     * @param existList valid link
+     * @param existLinkList valid link list
+     * @param existTableList valid table link
+     * @param existTableLinkList valid table link list
+     */
     public ValidTableLink(Vector<String> existList, String [] existLinkList, Vector<String> existTableList, String [] existTableLinkList) {
         super(existList, existLinkList);
         ValidTableLink.existTableList = existTableList;
         ValidTableLink.existTableLinkList = existTableLinkList;
     }
 
+    /**
+     * get existTableList from existLink
+     * @return existTableList
+     */
     public Vector<String> getExistTableList() {
         return existTableList;
     }
 
+    /**
+     * get existTableLink from existLink
+     * @return existTableLinkList;
+     */
     public String [] getExistTableLinkList() {
         return existTableLinkList;
     }
