80c9935989ee0e818e4558a5f2a5b168e1179b37
limxinyii <47627068+limxinyii@users.noreply.github.com>
2019-05-18 16:46:06 +0800

    Update KedahPlayers.java

--- src/main/java/com/groupProject/KedahPlayers.java
+++ src/main/java/com/groupProject/KedahPlayers.java
@@ -5,47 +5,62 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 
 import java.util.Properties;
-import java.util.concurrent.Callable;
 
-public class KedahPlayers implements Callable {
+/**
+ * @author Lim Xin Yi
+ * @version 1.0
+ * @since 2019-04-25
+ * Create KedahPlayers class from CheckValidTable and implements with Runnable
+ */
 
-    private String link;
+public class KedahPlayers extends CheckValidTable implements Runnable {
+    private String link2;
 
-    public KedahPlayers(String link) {
-        this.link = link;
+    public KedahPlayers() {
     }
 
+    public KedahPlayers(String link) {
+        super(link);
+        link2 = link;
+    }
+
+    /**
+     * get all kedah state players form valid link and read from properties file
+     * @param  link2 is ValidTableLink
+     * display the result based on the winning points and category
+     */
     @Override
-    public String call() {
+    public void run() {
         PropertyFileWriting obj = new PropertyFileWriting();
         Properties write = obj.setProperties();
         String prop = write.getProperty("State");
 
-        Document doc;
-        try {
-            doc = Jsoup.connect(link).get();
+        if (validTable(link2)) {
+            Document doc;
+            try {
+                doc = Jsoup.connect(link2).get();
 
-            //get category
-            String title = doc.title();
-            int scrape = title.indexOf("9");
-            String category = title.substring(scrape + 1).replace("(", "").replace(")", "");
+                String title = doc.title();
+                int scrape = title.indexOf("9");
+                String category = title.substring(scrape + 1).replace("(", "").replace(")", "");
 
-            for (Element row : doc.select("table.CRs1 tr")) {
-                final String rk = row.select("td:nth-of-type(1)").text();
-                final String sno = row.select("td:nth-of-type(2)").text();
-                final String name = row.select("td:nth-of-type(4)").text();
-                final String rtg = row.select("td:nth-of-type(6)").text();
-                final String state = row.select("td:nth-of-type(7)").text();
-                final String pointer = row.select("td:nth-of-type(8)").text();
+                for (Element row : doc.select("table.CRs1 tr")) {
+                    final String rk = row.select("td:nth-of-type(1)").text();
+                    final String sno = row.select("td:nth-of-type(2)").text();
+                    final String name = row.select("td:nth-of-type(4)").text();
+                    final String rtg = row.select("td:nth-of-type(6)").text();
+                    final String state = row.select("td:nth-of-type(7)").text();
+                    final String pointer = row.select("td:nth-of-type(8)").text();
 
-                if (state.equals(prop)) {
-                    String format = "| %-5s | %-5s | %-35s | %-8s | %-8s | %-8s | %-8s |\n";
-                    System.out.format(format, rk, sno, name, rtg, state, pointer, category);
+                    if (state.equals(prop)) {
+                        String format = "| %-5s | %-5s | %-35s| %-8s| %-8s| %-8s| %-8s|\n";
+                        System.out.format(format, rk, sno, name, rtg, state, pointer, category);
+                    }
                 }
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-        } catch (Exception e) {
-            e.printStackTrace();
         }
-        return link;
     }
 }
+
